3f9578b91c944d97da2d8e06c9989529388a1ab4
khadijaAwan5 <khadijasafdar567@gmail.com>
2020-07-17 20:50:34 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
+import useWebAnimations from "@wellyshen/use-web-animations";
 
 function App() {
+ // const element = useRef(null);
+
+  const { ref, playState,getAnimation } = useWebAnimations({
+    keyframes: [
+      {transform: "translate(0,0)"},
+      {transform: "translate(600%,0)"},
+    ],
+    timing: {
+      //delay: 2000, // Start with a 500ms delay
+      duration: 3000, // Run for 1000ms
+      iterations: Infinity, // Repeat once
+      direction: "alternate", // Run the animation forwards and then backwards
+      easing: "ease-in-out", // Use a fancy timing function
+    },
+    onReady: ({ playState, animate, animation }) => {
+      // Triggered when the animation is ready to play (Google Chrome: available in v84+)
+    },
+    onUpdate: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the running state or changes state
+    },
+    onFinish: ({ playState, animate, animation }) => {
+      // Triggered when the animation enters the finished state (Google Chrome: available in v84+)
+    },
+    // More useful options...
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <div className="target" ref={ref}> </div>
+     current animation state: {playState}
+     <button onClick={() => getAnimation().pause()}>Pause</button>
+     <button onClick={() => getAnimation().play()}>play</button>
     </div>
   );
 }
